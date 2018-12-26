107ce4ac76ea8ca8ae9919a4742b85523e794be0
Ishmael Rogers <41645540+IshmaelRogers@users.noreply.github.com>
2018-12-26 17:44:52 -0500

    Update and rename ReadMe.md to Hello_World.cpp

---
+++ code/Hello_World.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Allows the program to feature input/output capabilities
+using namespace std; // Designates the standard (std) namespace to be used throughout the program
+
+int main() // The starting point for all C++ programs 
+{          // Everything located inside the following curly braces will serve as out main code
+
+cout << "Hello, World!" ; // "cout" tells the complier that you would like to display everything after the "<<" on the screen
+return 0;                 // Specifies that the return type. In this case, there isn't one
+
+}                         // Closing curly braces that end the main program 
